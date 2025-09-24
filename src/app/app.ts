fc1f59820ec40dffcc27f37321c242c1daf30c25
Everton Ferreira <iseverton2024@gmail.com>
2025-09-24 18:39:03 -0300

    feat(employee-list): implement async pipe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RequestTest } from "./components/request-test/request-test";
-import { EmployeeList } from "./features/employee/employee-list/employee-list";
+import { EmployeeList } from './features/employee/employee-list/employee-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RequestTest, EmployeeList],
+  imports: [RouterOutlet, EmployeeList],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('workshop-manage-frontend');
--- src/app/features/employee/employee-list/employee-list.ts
+++ src/app/features/employee/employee-list/employee-list.ts
@@ -1,19 +1,24 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { EmployeeItem } from '../employee-item/employee-item';
 import { HttpClient } from '@angular/common/http';
+import { AsyncPipe } from '@angular/common';
+import { map, Observable } from 'rxjs';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-list',
-  imports: [EmployeeItem],
+  imports: [EmployeeItem, AsyncPipe],
   templateUrl: './employee-list.html',
   styleUrl: './employee-list.css',
 })
-export class EmployeeList implements OnInit {
+export class EmployeeList {
   private http = inject(HttpClient);
-  employee_list$ = [];
-  ngOnInit(): void {
-    this.http.get<any>('https://localhost:7289/api/Employee').subscribe((res) => {
-      this.employee_list$ = res.data;
-    });
+  employee_list$!: Observable<Employee[]>;
+
+  constructor() {
+    this.employee_list$ = this.http
+      .get<{ data: Employee[] }>('https://localhost:7289/api/Employee')
+      .pipe(map((response) => response.data));
+    console.log(this.employee_list$);
   }
 }
