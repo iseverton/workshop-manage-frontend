ceec099b270e76ff5655f11c83e6a6b219053db5
Everton Ferreira <iseverton2024@gmail.com>
2025-09-25 16:18:00 -0300

    feat(navigation): implement tab-menu component

---
+++ src/app/core/components/tab-menu/tab-menu.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TabMenu } from './tab-menu';
+
+describe('TabMenu', () => {
+  let component: TabMenu;
+  let fixture: ComponentFixture<TabMenu>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TabMenu]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TabMenu);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/core/components/tab-menu/tab-menu.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-tab-menu',
+  imports: [RouterLink],
+  templateUrl: './tab-menu.html',
+  styleUrl: './tab-menu.css',
+})
+export class TabMenu {}
