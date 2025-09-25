e1fdc92cb1d1cd386d5891a0a5cbcc4a31f00ebb
Everton Ferreira <iseverton2024@gmail.com>
2025-09-25 16:30:46 -0300

    feat(tab-menu): implement active state styling using routerLinkActive

--- src/app/core/components/tab-menu/tab-menu.ts
+++ src/app/core/components/tab-menu/tab-menu.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-tab-menu',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './tab-menu.html',
   styleUrl: './tab-menu.css',
 })
