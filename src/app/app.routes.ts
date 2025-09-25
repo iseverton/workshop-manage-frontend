87813f0d334902136a5bd0802c9154923cf88b6e
Everton Ferreira <iseverton2024@gmail.com>
2025-09-25 16:40:37 -0300

    feat(routing): implement application routes configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { EmployeeList } from './features/employee/employee-list/employee-list';
+import { WorkshopItem } from './features/workshop/workshop-item/workshop-item';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'employee',
+    pathMatch: 'full',
+  },
+  {
+    path: 'employee',
+    component: EmployeeList,
+  },
+  {
+    path: 'workshop',
+    component: WorkshopItem,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeList } from './features/employee/employee-list/employee-list';
+import { TabMenu } from './core/components/tab-menu/tab-menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeList],
+  imports: [RouterOutlet, TabMenu],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
